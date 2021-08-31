d4016bb124f509a67e246bb7fffdea27bbeb93b7
康营营 <yykang@huohuaschool.com>
2021-08-31 19:51:54 +0800

    add for: 增加设备梳理统计组件页面实现

--- src/components/Statistics/Other/DeviceSummary/DeviceSummary.vue
+++ src/components/Statistics/Other/DeviceSummary/DeviceSummary.vue
@@ -5,7 +5,52 @@
       <span class="text">设备数量统计</span>
     </div>
 
-    <div class="chart-container"></div>
+    <div class="chart-container">
+      <ul class="device-count-list">
+        <li class="item">
+          <div class="icon"></div>
+          <div class="meta">
+            <p class="count">5</p>
+            <p class="name">塔式起重机</p>
+          </div>
+        </li>
+        <li class="item">
+          <div class="icon"></div>
+          <div class="meta">
+            <p class="count">2</p>
+            <p class="name">铲运机</p>
+          </div>
+        </li>
+        <li class="item">
+          <div class="icon"></div>
+          <div class="meta">
+            <p class="count">5</p>
+            <p class="name">混凝土搅拌运输车</p>
+          </div>
+        </li>
+        <li class="item">
+          <div class="icon"></div>
+          <div class="meta">
+            <p class="count">5</p>
+            <p class="name">翻斗车</p>
+          </div>
+        </li>
+        <li class="item">
+          <div class="icon"></div>
+          <div class="meta">
+            <p class="count">4</p>
+            <p class="name">物料提升机</p>
+          </div>
+        </li>
+        <li class="item">
+          <div class="icon"></div>
+          <div class="meta">
+            <p class="count">2</p>
+            <p class="name">挖掘机</p>
+          </div>
+        </li>
+      </ul>
+    </div>
 
   </div>
 </template>
@@ -22,5 +67,44 @@ export default class DeviceSummary extends mixins(DeviceSummaryViewModel){
   .device-summary{
     width: 412px;
     height: 170px;
+  }
+  .device-summary .device-count-list{
+    padding: 17px 4px 17px 4px;
+    list-style: none;
+    display: grid;
+    grid-template-columns: repeat(3, minmax(100px, 1fr));
+    grid-gap: 18px;
+  }
+  .device-count-list .item{
+    height: 40px;
+    display: flex;
+  }
+  .item .icon{
+    width: 40px;
+    height: 40px;
+    background: #08D4BA;
+  }
+  .item .meta{
+    margin-left: 14px;
+  }
+  .meta .count{
+    font-size: 25px;
+    font-weight: bold;
+    color: #FFD304;
+    line-height: 24px;
+  }
+  .meta .name{
+    max-width: 65px;
+    font-size: 13px;
+    font-weight: 500;
+    color: #CFE1FB;
+    line-height: 24px;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 1;/*要显示的行数*/
+    -webkit-box-orient: vertical;
   }
 </style>
